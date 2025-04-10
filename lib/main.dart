54fd3417170fe8a89fcf16a58b8bcd17a8a6082b
masihur <masihur@evidentbd.com>
2025-04-10 12:56:54 +0600

    Price Remove

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,6 @@ import 'package:safe_choice/produc_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  // await Supabase.initialize(
-  //   url: 'https://nwdncjhdgkksvollpgsw.supabase.co',
-  //   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZG5jamhkZ2trc3ZvbGxwZ3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMTUzMTMsImV4cCI6MjA1OTc5MTMxM30.7CGPTFjI7WwxoN2Xmr-imIhobpqs9yaJX1JGwi9uGfI',
-  // );
   runApp(const MyApp());
 }
 
